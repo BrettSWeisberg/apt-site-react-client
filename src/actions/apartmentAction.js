b7687dfefd15412c41bfaa902bbc3d33d6d5f7d0
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-23 11:53:40 -0400

    First commit

---
+++ src/actions/apartmentAction.js
@@ -0,0 +1,8 @@
+// export function fetchCats() {
+//   return (dispatch) => {
+//     dispatch({ type: 'LOADING_CATS' });
+//     return fetch('http://localhost:4000/db')
+//       .then(response => response.json())
+//       .then(cats => dispatch({ type: 'FETCH_CATS', payload:cats.images}));
+//   };
+// }
