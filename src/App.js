761881c583bfaeedf12020b1cf4189f9d84bd4b1
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-25 21:38:56 -0400

    Routes set home and create

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import BuildingsContainer from './containers/BuildingsContainer';
+import HomePageContainer from './containers/HomePageContainer';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BuildingsContainer />
-      
+        <Router>
+          <Route path='/' component={HomePageContainer} />
+          <Route path='/building/new' component={BuildingsContainer} />
+        </Router>
       </div>
     );
   }
