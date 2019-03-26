e62ef7757fd5b58a2b7574ff3c5d54860c99ebad
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-25 21:47:25 -0400

    Home component set

---
+++ src/components/buildings/HomeBuilding.js
@@ -0,0 +1,26 @@
+// import React, { Component } from 'react';
+// import ApartmentsContainer from '../../containers/ApartmentsContainer'
+//
+// class Building extends Component {
+//
+//
+//   render() {
+//     const { building } = this.props;
+//
+//     return (
+//
+//       <div>
+//         <li>
+//           <div>Name:{building.name}</div>
+//           <div>address:{building.address}</div>
+//           <div>description:{building.description}</div>
+//           <div>Units:{building.units}</div>
+//           <button onClick={() => this.props.delete(building.id)}> Delete </button>
+//           <ApartmentsContainer building={building}/>
+//         </li>
+//       </div>
+//     );
+//   }
+// };
+//
+// export default Building;
