8f4f19aa8a11d94a1f36efaecb4b1d9d0860e7d7
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-25 21:56:34 -0400

    Homepage in progres

--- src/components/buildings/HomeBuilding.js
+++ src/components/buildings/HomeBuilding.js
@@ -1,26 +1,30 @@
-// import React, { Component } from 'react';
-// import ApartmentsContainer from '../../containers/ApartmentsContainer'
-//
-// class Building extends Component {
-//
-//
-//   render() {
-//     const { building } = this.props;
-//
-//     return (
-//
-//       <div>
-//         <li>
-//           <div>Name:{building.name}</div>
-//           <div>address:{building.address}</div>
-//           <div>description:{building.description}</div>
-//           <div>Units:{building.units}</div>
-//           <button onClick={() => this.props.delete(building.id)}> Delete </button>
-//           <ApartmentsContainer building={building}/>
-//         </li>
-//       </div>
-//     );
-//   }
-// };
-//
-// export default Building;
+import React, { Component } from 'react';
+//import ApartmentsContainer from '../../containers/ApartmentsContainer'
+
+class HomeBuilding extends Component {
+
+  renderBuildings = () => {
+    return  this.props.buildings.map(building =>
+     <Building key={building.id} building={building} />)
+  }
+
+  render() {
+    const { building } = this.props;
+
+    return (
+
+      <div>
+        <li>
+          <div>Name:{building.name}</div>
+          <div>address:{building.address}</div>
+          <div>description:{building.description}</div>
+          <div>Units:{building.units}</div>
+        //  <button onClick={() => this.props.delete(building.id)}> Show Apartments </button>
+      //    <ApartmentsContainer building={building}/>
+        </li>
+      </div>
+    );
+  }
+};
+
+export default HomeBuilding;
