c35a4b9116bd5bff405c7218cfa5c6cdb3021f76
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-27 06:11:33 -0400

    Apartment form update

--- src/components/apartments/Apartment.js
+++ src/components/apartments/Apartment.js
@@ -12,7 +12,12 @@ class Apartment extends Component {
     return (
       <div>
         <li>
-          {this.props.apartment.name}
+          <div>Name: {this.props.apartment.name}</div>
+          <div>Square Feet: {this.props.apartment.sf}</div>
+          <div>Baths: {this.props.apartment.baths}</div>
+          <div>rooms: {this.props.apartment.rooms}</div>
+          <div>description: {this.props.apartment.description}</div>
+          <div>price: {this.props.apartment.price}</div>
         </li>
         <button onClick={this.handleOnClick}> x </button>
       </div>
