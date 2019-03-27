824225309cae06d43a21ea6b5a02a598865a5f8a
Brett Weisberg <weisberg.brett@gmail.com>
2019-03-27 18:50:24 -0400

    Basic style set

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+
 import React, { Component } from 'react';
 import BuildingsContainer from './containers/BuildingsContainer';
 import HomePageContainer from './containers/HomePageContainer';
 import AboutUs from './components/static/AboutUs';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
+import 'bulma/css/bulma.css'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <ul className="navbar-nav mr-auto">
-            <li><Link to={'/'} className="nav-link"> Home </Link></li>
-            <li><Link to={'/building/new'} className="nav-link">Create Building</Link></li>
-            <li><Link to={'/aboutus'} className="nav-link">About Us</Link></li>
-          </ul>
-          </nav>
+        <nav>
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <div class="navbar-item"><Link to={'/'} className="nav-link">Home</Link></div>
+            <div class="navbar-item"><Link to={'/building/new'} className="nav-link">Create Building</Link></div>
+            <div class="navbar-item"><Link to={'/aboutus'} className="nav-link">About Us</Link></div>
+            </div>
+          </div>
+         </nav>
           <Switch>
           <Route exact path='/' component={HomePageContainer} />
           <Route path='/building/new' component={BuildingsContainer} />
